df5229dc3f308b88c1aa4a5f8ae22f6033a6fb4c
Simon Clément <simon.clement98@yahoo.com>
2018-12-18 09:10:22 +0100

    Ajout du service

--- src/app/display-film/display-film.component.ts
+++ src/app/display-film/display-film.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {TableFilmService} from './display-film.service';
 @Component({
   selector: 'app-display-film',
   templateUrl: './display-film.component.html',
-  styleUrls: ['./display-film.component.css']
+  styleUrls: ['./display-film.component.css'],
+  providers: [TableFilmService]
 })
 export class DisplayFilmComponent implements OnInit {
 
+  backEndLocation: string;
   idFilm: number;
   name: string;
   description: string;
@@ -14,11 +17,17 @@ export class DisplayFilmComponent implements OnInit {
   year: number;
   comment: string;
   constructor(public dialogRef: MatDialogRef<DisplayFilmComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              public service: TableFilmService) {
       this.idFilm = data['idFilm'];
   }
 
   ngOnInit() {
+    this.initConfig();
   }
-
+  initConfig(): void {
+    this.service.getConfig().subscribe((data: any) => {
+      this.backEndLocation = data['calledURL'];
+    });
+  }
 }
---
+++ src/app/display-film/display-film.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Film } from '../class/film';
+
+@Injectable()
+export class TableFilmService {
+    constructor(private http: HttpClient) { }
+
+    configUrl = 'assets/config.json';
+
+
+    getConfig() {
+        return this.http.get(this.configUrl);
+    }
+}
